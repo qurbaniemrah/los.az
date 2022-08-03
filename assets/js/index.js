a109cc36656be90506d052207f73337e16a27e31
qurbaniemrah <qurbaniemrah@outlook.com>
2022-08-03 11:33:47 +0400

    categories section uzerinde isledim

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// const categoryListItem = document.querySelectorAll('.category-list-item');
+// const categoryListDrp = document.querySelectorAll('.category-list-dropdown');
+// const dropdownSortli = document.querySelectorAll('.dropdown-container sortLi');
+
+// categoryListItem.forEach(list => {
+//     for(let i =0; i<categoryListDrp.length; i++) {
+//        list.addEventListener('mouseover', (e)=> {
+//        if(e.target == list) {
+//         categoryListDrp[i].style.display = "block"
+//        }else {
+//         categoryListDrp[i].style.display = "none"
+//        }
+//        })
+//     }
+// });
