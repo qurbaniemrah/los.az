333e2c8eabe61f3c011ceba2b47902546919923d
qurbaniemrah <qurbaniemrah@outlook.com>
2022-08-04 21:37:47 +0400

    office tool section uzerinde isledim

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,6 +57,25 @@ $(document).ready(function () {
 
 // menu and category
 
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// swiper
+
 
 
 
