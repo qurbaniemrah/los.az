fa36e70ea69d3133195af5c11171fb331ddaf309
qurbaniemrah <qurbaniemrah@outlook.com>
2022-08-07 22:45:13 +0400

    carousel hissede isledim

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,26 +77,48 @@ var swiper = new Swiper(".mySwiper", {
 // swiper
 
 // owl carousel
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  autoPlay:true,
-  margin:10,
-  nav:true,
-  rewindNav:false,
+// $('.owl-carousel').owlCarousel({
+  // loop:true,
+  // margin:10,
+  // nav:true,
+  // rewindNav:false,
+  
+  
 
+//   responsive:{
+//       0:{
+//           items:1
+//       },
+//       600:{
+//           items:3
+//       },
+//       1000:{
+//           items:5
+//       }
+//   }
+// })
 
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  center: true,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplaySpeed: 1000,
+  responsiveClass: true,
   responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
-})
+    0:{
+        items: 1 
+    },
+    600:{
+        items: 3
+    },
+    1200:{
+        items: 5
+    }
+}
+});
 // owl carousel
 
 
